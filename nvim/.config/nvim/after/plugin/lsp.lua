124f6719d646cc662c27a3b4c327c4d1d60a8eea
John Johnson <johnernaut89@gmail.com>
2022-08-14 08:37:52 -0500

    clean up LSP settings

--- nvim/.config/nvim/after/plugin/lsp.lua
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -1,10 +1,4 @@
--- Ruby
-require('lspconfig').solargraph.setup{
-  capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
-}
-
-require('lspconfig').sorbet.setup{}
-
+-- LSP settings
 local nvim_lsp = require('lspconfig')
 
 -- Use an on_attach function to only map the following keys
@@ -34,18 +28,6 @@ local on_attach = function(client, bufnr)
   buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
   buf_set_keymap("n", "<space>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
 
-end
-
--- Use a loop to conveniently call 'setup' on multiple servers and
--- map buffer local keybindings when the language server attaches
-local servers = { "solargraph" }
-for _, lsp in ipairs(servers) do
-  nvim_lsp[lsp].setup {
-	on_attach = on_attach,
-	flags = {
-	  debounce_text_changes = 150,
-	}
-  }
 end
 
 -- nvim cmp
@@ -128,3 +110,23 @@ cmp.setup.cmdline(':', {
     { name = 'cmdline' }
   })
 })
+
+
+
+-- Ruby
+require('lspconfig').solargraph.setup{
+  capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities()),
+  settings = {
+      solargraph = {
+          autoformat = true,
+          completion = true,
+          diagnostic = true,
+          folding = true,
+          references = true,
+          rename = true,
+          symbols = true
+      }
+  }
+}
+
+require('lspconfig').sorbet.setup{}
