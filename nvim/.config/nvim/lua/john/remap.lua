64f596403087fa63180a0a72880cb215902b72dc
John Johnson <johnernaut89@gmail.com>
2022-08-15 09:37:12 -0500

    neovim config update

--- nvim/.config/nvim/lua/john/remap.lua
+++ nvim/.config/nvim/lua/john/remap.lua
@@ -43,3 +43,11 @@ nnoremap("<leader>j", "<cmd>lprev<CR>zz")
 
 nnoremap("<leader>s", ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>")
 nnoremap("<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+
+-- Easier pane navigation
+vim.cmd([[
+nmap <silent> <c-k> :wincmd k<CR>
+nmap <silent> <c-j> :wincmd j<CR>
+nmap <silent> <c-h> :wincmd h<CR>
+nmap <silent> <c-l> :wincmd l<CR>
+]])
