74a59147f7f7fcdc2ee947dda3281fe4660bf82a
John Johnson <johnernaut89@gmail.com>
2022-12-21 07:08:42 -0600

    update lsp

--- nvim/.config/nvim/after/plugin/lsp.lua
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -1,12 +1,37 @@
 local lsp = require("lsp-zero")
 
 lsp.preset("recommended")
-lsp.setup()
+
+lsp.ensure_installed({
+  'tsserver',
+  'eslint',
+  'sumneko_lua',
+  'rust_analyzer',
+})
+
+-- Fix Undefined global 'vim'
+lsp.configure('sumneko_lua', {
+    settings = {
+        Lua = {
+            diagnostics = {
+                globals = { 'vim' }
+            }
+        }
+    }
+})
 
 lsp.on_attach(function(client, bufnr)
     local opts = {buffer = bufnr, remap = false}
     local bind = vim.keymap.set
 
-    bind("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+    bind("n", "<leader>vca", vim.lsp.buf.code_action, opts)
+    bind("n", "<leader>vrr", vim.lsp.buf.references, opts)
+    bind("n", "<leader>vrn", vim.lsp.buf.rename, opts)
     bind("n", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
 end)
+
+lsp.setup()
+
+vim.diagnostic.config({
+    virtual_text = true,
+})
